6e5b3bbf40d767d16f7c389da77aa5cc65438df0
jmart5 <93228623+jmart5@users.noreply.github.com>
2023-06-13 20:04:57 -0400

    L5 - Nesting Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home'; 
 
 function App() {
-  const title = 'welcome to the new blog';
-  const likes = 50;
-  const link = "https://www.google.com";
-
   return (
     <div className="App">
-      <div className="App">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={ link }>Google Homepage</a>
+      <Navbar />
+      <div className="content">
+        <Home />
       </div>
     </div>
   );
