0203a28fd80094686beb9c64087e01d90ea2d57e
jmart5 <93228623+jmart5@users.noreply.github.com>
2023-06-14 19:08:10 -0400

    L7 - events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    //e provides info about the event.
+    const handleClick = (e) => {
+        console.log('hello, ninjas', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello, ' + name, e.target);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('Mario', e)}>Click me again</button>
         </div>
       );
 }
