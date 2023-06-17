7300390a4c1205308e3e2338164171ded260cc6d
jmart5 <93228623+jmart5@users.noreply.github.com>
2023-06-16 21:21:36 -0400

    L14 - useEffect added

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -12,6 +12,12 @@ const Home = () => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
+
+  {/*useEffect is useful to run code that you need to run on every render*/}
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  })
 
   return (
     <div className="home">
