7ad150d84aa05b09419deeee7b6fb58161a9f9e9
jmart5 <93228623+jmart5@users.noreply.github.com>
2023-06-16 21:16:29 -0400

    L13 - Pass functions as Props

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,23 @@
-import { useState } from "react"; 
-import BlogList from './BlogList';
+import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-   const [blogs, setBlogs] = useState([
+  const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
-    return (
-        <div className="home">
-            {/*use props - a way to pass data from a parent component to a child component*/}
-            <BlogList blogs={blogs} title="All Blogs!" />     
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's blogs" />
-         </div>
-      );
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+    </div>
+  );
 }
  
 export default Home;
