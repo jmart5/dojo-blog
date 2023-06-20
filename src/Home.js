5200b6c246332ecb3bc52989c9cc7a43a2736c70
jmart5 <93228623+jmart5@users.noreply.github.com>
2023-06-19 20:39:23 -0400

    L16 - JSON server

--- src/Home.js
+++ src/Home.js
@@ -8,8 +8,6 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
-  const [name, setName] = useState('mario');
-
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -18,14 +16,11 @@ const Home = () => {
   {/*useEffect is useful to run code that you need to run on every render*/}
   useEffect(() => {
     console.log('use effect ran');
-    console.log(name);
-  }, [name])
+  }, [])
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('luigi')}>change name</button>
-      <p>{ name }</p>
     </div>
   );
 }
