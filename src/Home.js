0f5a972dfe6317253be39e450844324850d35d77
jmart5 <93228623+jmart5@users.noreply.github.com>
2023-06-19 20:24:50 -0400

    L15 - add dependency array as 2nd arg to useEffect

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -16,12 +18,14 @@ const Home = () => {
   {/*useEffect is useful to run code that you need to run on every render*/}
   useEffect(() => {
     console.log('use effect ran');
-    console.log(blogs);
-  })
+    console.log(name);
+  }, [name])
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{ name }</p>
     </div>
   );
 }
